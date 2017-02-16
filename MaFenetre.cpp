a5f65c84d9beb6f267ed1446e1013874b428ff7d
pgendreau <philippe.gendreau.2@ulaval.ca>
2017-02-15 21:06:40 -0500

    Added QCustomPlot to MaFenetre. Please delete MaFenetre and transfert example to MainWindow.

--- MaFenetre.cpp
+++ MaFenetre.cpp
@@ -4,15 +4,41 @@
 #include "PokerTextFile.h"
 #include <string>
 #include <QDebug>
+#include "qcustomplot.h"
+#include <QHBoxLayout>
 
 
 MaFenetre::MaFenetre() : QWidget()
 {
     m_fileName = "";
-    setFixedSize(230, 120);
+    setFixedSize(400,400);
 
     m_boutonDialogue = new QPushButton("Ouvrir la boîte de dialogue", this);
-    m_boutonDialogue->move(40, 50);
+
+    QCustomPlot* pokerPlot = new QCustomPlot(this);
+    //pokerPlot->setGeometry(0,0,400,400);
+
+    QVector<double> x(101), y(101); // initialize with entries 0..100
+    for (int i=0; i<101; ++i)
+    {
+      x[i] = i/50.0 - 1; // x goes from -1 to 1
+      y[i] = x[i]*x[i]; // let's plot a quadratic function
+    }
+    // create graph and assign data to it:
+    pokerPlot->addGraph();
+    pokerPlot->graph(0)->setData(x, y);
+    // give the axes some labels:
+    pokerPlot->xAxis->setLabel("x");
+    pokerPlot->yAxis->setLabel("y");
+    // set axes ranges, so we see all data:
+    pokerPlot->xAxis->setRange(-1, 1);
+    pokerPlot->yAxis->setRange(0, 1);
+    pokerPlot->replot();
+
+    QVBoxLayout* layout = new QVBoxLayout();
+    layout->addWidget(pokerPlot);
+    layout->addWidget(m_boutonDialogue);
+    setLayout(layout);
 
     QObject::connect(m_boutonDialogue, SIGNAL(clicked()), this, SLOT(ouvrirDialogue()));
 }
